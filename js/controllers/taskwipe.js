3fed16e24831c2bb2f9756fcb55d05269e7ffb4d
iace9 <amost076@uottawa.ca>
2016-07-18 15:40:15 -0400

    Added routing

--- js/controllers/taskwipe.js
+++ js/controllers/taskwipe.js
@@ -1,8 +1,24 @@
 (function() {
 	
-	//Defining our app, calling it taskWipe
-	var app = angular.module('taskWipe', ['dir-elements']);
-  
+	/*Defining our app, calling it taskWipe.
+		- dir-elements.js is injected to inserts snipets of code into the HTML keeping everything consistent and more organized.
+		- ngRoutes redirects the user to different pages depending where they are in the site		
+	*/	
+	var app = angular.module('taskWipe', ['dir-elements','ngRoute']).config(function($routeProvider){
+		
+		//Declaring routes
+		$routeProvider
+			.when( '/', {
+			templateUrl: '/templates/pages/main/index.html'
+			})
+			.when( '/login', {
+			templateUrl: '/templates/pages/login/index.html'
+			})
+			.otherwise({
+			redirectTo: '/templates/pages/main/index.html'
+			})
+		});
+    
 	app.controller('taskController', ['$http', function($http){
 		var collection = this;
 		collection.tasks = [];
